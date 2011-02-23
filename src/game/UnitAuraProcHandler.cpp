8fa54e868f85f3cdc7cb7d1e5f3f9b2dde4707c0
pasdVn <pasdVn3@gmx.de>
2011-02-23 23:16:09 +0100

    [IOV87] Fixed "Lock and Load" procs.
    Note: The proc flags provided in the submodule are outdated!
    
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -4150,8 +4150,7 @@ SpellAuraProcResult Unit::HandleProcTriggerSpellAuraProc(Unit *pVictim, uint32 d
         {
             // Proc only from trap activation (from periodic proc another aura of this spell)
             // because some spells have both flags (ON_TRAP_ACTIVATION and ON_PERIODIC), but should only proc ON_PERIODIC!!
-            if (!(procFlags & PROC_FLAG_ON_TRAP_ACTIVATION) || !procSpell ||
-                !(procSpell->SchoolMask & SPELL_SCHOOL_MASK_FROST) || !roll_chance_i(triggerAmount))
+            if (!(procFlags & PROC_FLAG_ON_TRAP_ACTIVATION) || !procSpell || !roll_chance_i(triggerAmount))
                 return SPELL_AURA_PROC_FAILED;
             break;
         }
