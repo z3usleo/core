996f4ded8f4dff2b2de351a553f89a5480a0dcda
fgenesis <fgenesis@getmangos.com>
2011-03-13 03:21:27 +0100

    [IOV137] fixed Icy Touch, Deadly Poison, Hemorrhage stacking from multiple targets (maybe more spells are affected too)
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4387,6 +4387,11 @@ bool Unit::AddSpellAuraHolder(SpellAuraHolder *holder)
                     case SPELL_AURA_PERIODIC_MANA_LEECH:
                     case SPELL_AURA_OBS_MOD_ENERGY:
                     case SPELL_AURA_POWER_BURN_MANA:
+                    case SPELL_AURA_MOD_DAMAGE_FROM_CASTER: // required for Serpent Sting (blizz hackfix?)
+                    case SPELL_AURA_MOD_ATTACKER_SPELL_AND_WEAPON_CRIT_CHANCE: // for Deadly Poison
+                    case SPELL_AURA_MOD_MELEE_HASTE:  // for Icy Touch
+                    case SPELL_AURA_MOD_RANGED_HASTE: // for Icy Touch
+                    case SPELL_AURA_MOD_DAMAGE_TAKEN: // for Hemorrhage
                         break;
                     case SPELL_AURA_PERIODIC_ENERGIZE:      // all or self or clear non-stackable
                     default:                                // not allow
