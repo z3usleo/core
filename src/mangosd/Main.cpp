62e4be13bb4b5215131c6190c3341756248048f4
GMHAZE <iceis2@hotmail.com>
2011-03-20 15:22:07 -0400

    system message and artwork

--- src/mangosd/Main.cpp
+++ src/mangosd/Main.cpp
@@ -165,17 +165,20 @@ extern int main(int argc, char **argv)
     sLog.outString( "%s [world-daemon]", _FULLVERSION(REVISION_DATE,REVISION_TIME,REVISION_NR,REVISION_ID) );
     sLog.outString( "<Ctrl-C> to stop.\n\n" );
 
-    sLog.outTitle( "MM   MM         MM   MM  MMMMM   MMMM   MMMMM");
-    sLog.outTitle( "MM   MM         MM   MM MMM MMM MM  MM MMM MMM");
-    sLog.outTitle( "MMM MMM         MMM  MM MMM MMM MM  MM MMM");
-    sLog.outTitle( "MM M MM         MMMM MM MMM     MM  MM  MMM");
-    sLog.outTitle( "MM M MM  MMMMM  MM MMMM MMM     MM  MM   MMM");
-    sLog.outTitle( "MM M MM M   MMM MM  MMM MMMMMMM MM  MM    MMM");
-    sLog.outTitle( "MM   MM     MMM MM   MM MM  MMM MM  MM     MMM");
-    sLog.outTitle( "MM   MM MMMMMMM MM   MM MMM MMM MM  MM MMM MMM");
-    sLog.outTitle( "MM   MM MM  MMM MM   MM  MMMMMM  MMMM   MMMMM");
-    sLog.outTitle( "        MM  MMM http://getmangos.com");
-    sLog.outTitle( "        MMMMMM\n\n");
+    sLog.outTitle( "               P   R   O   J   E   C   T __ The Infinity & IOV project __      ");	
+    sLog.outTitle( "              A Special Designed Project To Be The Best           I            ");	
+    sLog.outTitle( ":::::::::      :::     :::        :::     ::::::::   :::::::::     N   P       ");
+    sLog.outTitle( ":+:          :+: :+:   :+:        :+:     :+:        :+:    :+:     F   R      ");
+    sLog.outTitle( "+:+         +:+   +:+  +:+        +:+     +:+        +:+     +:+     I   O     ");
+    sLog.outTitle( "+:+::::::   #++:++#++: +#+        +#+     +#++:++#   +#+     +#+      N   J    ");
+    sLog.outTitle( "+:+        +#+     +#+ +#+        +#+     +#+        +#+     +#+  &    I   E   ");
+    sLog.outTitle( "#+#        #+#     #+# #+#        #+#     #+#        #+#     #+#   I    T   C  ");
+    sLog.outTitle( "###        ###     ### #######    ####### ########   ###     ###    O    Y   T ");
+    sLog.outTitle( "Special Made Core & SD2 By and for The Players                       V         ");	
+    sLog.outTitle( "Github.com/fallenangelX/(Retired Developer From Hellscream & Dark_Ice)         ");
+    sLog.outTitle( "Github.com/singlem/(Retired Developer From Hellscream & Dark_Ice)              ");
+    sLog.outTitle( "Project The InFinity Project  Core Developed orginally for TheFallen_Server    ");
+
 
     sLog.outString("Using configuration file %s.", cfg_file);
 
